c88af362717f37c4d75eba55187e404451f2d658
Dhruv <mrdhruvbhuva07@gmail.com>
2021-02-05 12:45:58 -0800

    second commit

--- test/javascript/login.js
+++ test/javascript/login.js
@@ -1,14 +1,29 @@
 function login(){
 
-  var name=document.getElementById("name").value;
-  var password=document.getElementById("password").value;
+	var obja=new Object();
+	obja.name="Hiren";
+	obja.password="123456";
+  localStorage.setItem("Hiren",JSON.stringify(obja));
+  var return1=true;
+  var name=document.getElementById("name1").value;
 
-    if(name==localStorage.getItem("AdminName") && password==localStorage.getItem("AdminPassword"))
-	    {
-	        console.log("Move to Dasabord");	
-	    }else if(name==(JSON.parse(localStorage.getItem(localStorage.getItem(name)))).name && password==(JSON.parse(localStorage.getItem(localStorage.getItem(name)))).password)
-	    {
-            document.getElementById("form").setAttribute("action","Sub-User.html");
-	    }
+  var password=document.getElementById("Password").value;
 
+     if(name==localStorage.getItem("AdminName") && password==localStorage.getItem("AdminPassword"))
+     {
+         console.log("Admin Login");
+
+   }else if (name!=null && password!=null && name==(JSON.parse(localStorage.getItem(name))).name && password==(JSON.parse(localStorage.getItem(name))).password)
+   {
+        document.getElementById("form").setAttribute("action","Sub-User.html");
+
+   }
+   else{
+       console.log("incorrect User name and password !!!");
+       return1=false;
+   }
+
+  return return1;
 }
+
+console.log("hi");
